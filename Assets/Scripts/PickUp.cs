60b3bdcb0ba18ea39ddc183958cd878c29d2dbe3
Sampakk <samuli.kosonen@gmail.com>
2021-11-05 20:53:59 +0200

    Kommentit ja hypyn korjaus

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -7,15 +7,19 @@ public class PickUp : MonoBehaviour
     public Transform Destination;
     public Rigidbody CubeRb;
 
+
     void OnMouseDown()
     {
+        //These ones freeze and disable collider once you pick up the object
         GetComponent<BoxCollider>().enabled = false;
         GetComponent<Rigidbody>().useGravity = false;
         GetComponent<Rigidbody>().freezeRotation = true;
+        CubeRb.constraints = RigidbodyConstraints.FreezePosition;
 
+        //These put the object in to the "destination" and adds it as a child
         this.transform.position = Destination.position;
         this.transform.parent = GameObject.Find("Destination").transform;
-
+        //this one stops the momentum so it doesnt float away from your hands
         transform.position = Destination.position;
         CubeRb.velocity = Vector3.zero;
         CubeRb.angularVelocity = Vector3.zero;
@@ -23,10 +27,13 @@ public class PickUp : MonoBehaviour
     }
     void OnMouseUp()
     {
+        //These ones unfreeze when you drop it
         this.transform.parent = null;
         GetComponent<Rigidbody>().freezeRotation = false;
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<BoxCollider>().enabled = true;
-        
+        CubeRb.constraints = RigidbodyConstraints.None;
+
+
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,21 @@ public class PlayerMovement : MonoBehaviour
     public GameObject moveMe;
     Rigidbody rb;
     private float MovementSpeed = 10f;
-    private float gravity = -9.81f;
 
     public float jumpHeight = 3f;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.45f;
+    public LayerMask groundMask;
 
     
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();   
+
     }
 
     // Update is called once per frame
@@ -26,5 +30,16 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(Input.GetAxis("Horizontal"));
         Vector3 Movements = new Vector3(Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime);
         moveMe.transform.Translate(Movements);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        { 
+            rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+        }
+    
+    }
+    bool IsGrounded()
+    {
+        if (Physics.CheckSphere(groundCheck.position, groundDistance, groundMask)) return true;
+        return false;
     }
 }
