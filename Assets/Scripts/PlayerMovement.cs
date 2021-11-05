b4f17732f71e397ded86493e51a4f048f89dd926
teamsanteri <93781188+teamsanteri@users.noreply.github.com>
2021-11-05 17:29:22 +0200

    MovementScript

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private float MovementSpeed = 30f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 Movements = new Vector3(Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime);
+        gameObject.transform.Translate(Movements);
+
     }
 }
