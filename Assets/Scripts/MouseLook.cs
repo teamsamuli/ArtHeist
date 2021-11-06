cdc054a835a2a50bbf16282754aff1e4a5a5b5ab
Niko-Viitio <dnivi009@edu.xamk.fi>
2021-11-06 12:14:47 +0200

    upadted mouselook

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,33 +5,39 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     PickUp pickUp;
+    Rigidbody rb;
+
     public float mouseSensitivity = 100f;
     public GameObject destination;
-    public Transform cameraPos;
     public Transform playerBody;
     public LayerMask Objects;
+
+    float mouseX, mouseY;
     float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // lukitsee kursorin jos klikkaapeli‰ (tai on in focus)
+        //Get player rigidbody
+        rb = GetComponentInParent<Rigidbody>();
+
+        //Lock cursor
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //k‰ytt‰‰ unityn sis‰isi‰ hiiren ominaisuuksia
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        
+        //Get mouse movement
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+       
+        //Apply vertical rotation and clamp it
         xRotation -= mouseY;
-        // Lukitsee ettei katse mene yli ylimm‰n kohdan tai ali alimman kohdan
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        //Rotate player horizontally
         playerBody.Rotate(Vector3.up * mouseX);
 
         //Pick up item
@@ -61,7 +67,5 @@ public class MouseLook : MonoBehaviour
                 pickUp.ThrowItem(transform.forward, 10f);
             }
         }
-
     }
-
 }
