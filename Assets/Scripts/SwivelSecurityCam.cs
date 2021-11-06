3f035b836bc021af8775d816fe4da2a5d709beba
teamsanteri <93781188+teamsanteri@users.noreply.github.com>
2021-11-06 11:24:56 +0200

    Added comments to security cam rotation script

--- Assets/Scripts/SwivelSecurityCam.cs
+++ Assets/Scripts/SwivelSecurityCam.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class SwivelSecurityCam : MonoBehaviour
 {
+    /// <summary>
+    /// Used to measure time until next rotation
+    /// </summary>
     float endedRotation;
 
-    float timeDelayBetweenRotations = 10f;
+    /// <summary>
+    /// Time delay between rotations in seconds
+    /// </summary>
+    public float timeDelayBetweenRotations = 10f;
 
+    /// <summary>
+    /// Number of degrees the camera will rotate
+    /// </summary>
+    public float RotateByDegrees = 90f;
+
+    /// <summary>
+    /// Boolean used to know, which way to rotate the camera next
+    /// </summary>
     bool IsFacingLeft = false;
 
+    /// <summary>
+    /// Do not check the time when rotating the camera
+    /// </summary>
     bool isRotating = false;
 
+    /// <summary>
+    /// Variable to track how many degrees the camera has rotated to prevent overrotation
+    /// </summary>
     float RotatedDegrees = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        //gameObject.transform.Rotate(Vector3.up, -45, Space.World); // prepare for rotation by facing left
         IsFacingLeft = true;
         endedRotation = Time.time;
     }
@@ -28,36 +47,40 @@ public class SwivelSecurityCam : MonoBehaviour
         // Check if not rotating yet and enough time has passed after previous rotation
         if (!isRotating && Time.time - endedRotation > timeDelayBetweenRotations)
         {
-            isRotating = true;
-            Debug.Log("Started Rotating");
+            isRotating = true; // Time has passed, start rotating
+            //Debug.Log("Started Rotating");
         }
 
-        if (isRotating)
+        if (isRotating) // Camera is rotating
         {
-            float rotateBy = 90;
-
-            if (!IsFacingLeft) // if is not facing left, then reverse the rotation direction
-            {
-                rotateBy = rotateBy * -1;
-            }
-
-            float rotateAmount = rotateBy * Time.deltaTime;
-
-            RotatedDegrees = RotatedDegrees + rotateAmount;
-
-            // Slowly pan the camera to other side
-            gameObject.transform.Rotate(Vector3.up, rotateAmount, Space.World);
-
-            Debug.Log(RotatedDegrees.ToString());
-            if (RotatedDegrees > 90 || RotatedDegrees < -90)
-            {
-                Debug.Log("Finished Rotating");
-                isRotating = false;
-                endedRotation = Time.time;
-                IsFacingLeft = !IsFacingLeft; // Now set the direction to reverse next time
-                RotatedDegrees = 0; // Reset the rotation to prevent overrotating
-            }
+            RotateCamera();
         }
     }
 
+    private void RotateCamera()
+    {
+        float rotateBy = RotateByDegrees;
+
+        if (!IsFacingLeft) // if is not facing left, then reverse the rotation direction
+        {
+            rotateBy = rotateBy * -1;
+        }
+
+        float rotateAmount = rotateBy * Time.deltaTime; // Smooth rotation
+
+        RotatedDegrees = RotatedDegrees + rotateAmount; // Track the amount of rotated degrees to know when to stop
+
+        // Slowly pan the camera to other side
+        gameObject.transform.Rotate(Vector3.up, rotateAmount, Space.World);
+
+        //Debug.Log(RotatedDegrees.ToString());
+        if (RotatedDegrees > 90 || RotatedDegrees < -90)
+        {
+            //Debug.Log("Finished Rotating");
+            isRotating = false; // Not rotating anymore
+            endedRotation = Time.time; // Timestamp to know the end time
+            IsFacingLeft = !IsFacingLeft; // Reverse the rotation direction for next time
+            RotatedDegrees = 0; // Reset the rotation tracking to prevent overrotating
+        }
+    }
 }
