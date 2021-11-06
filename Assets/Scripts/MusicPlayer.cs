082e9023005b253f7f7c2ac82e818d290c06cd84
Niko-Viitio <dnivi009@edu.xamk.fi>
2021-11-07 00:16:36 +0200

    Toimii!

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class MusicPlayer : MonoBehaviour
 {
     AudioSource audioSrc;
+    GuardSpawner guardSpawner;
 
-    public AudioClip notSpottedMusic;
-    public AudioClip spottedMusic;
+    public AudioClip spyMusic;
+    public AudioClip chaseMusic;
+
+    float updateInterval = 0.5f;
+    float lastTimeUpdated;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSrc = GetComponent<AudioSource>();
+        guardSpawner = FindObjectOfType<GuardSpawner>();
 
-        audioSrc.clip = notSpottedMusic;
+        audioSrc.clip = spyMusic;
         audioSrc.loop = true;
         audioSrc.Play();
     }
@@ -22,6 +27,25 @@ public class MusicPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Check if need to change music
+        if (Time.time >= lastTimeUpdated + updateInterval)
+        {
+            lastTimeUpdated = Time.time;
+
+            ChangeSong(guardSpawner.IsPlayerSpotted());
+        }
+    }
+
+    void ChangeSong(bool playChaseMusic)
+    {
+        //Dont change if already right song is playing
+        if (playChaseMusic && audioSrc.clip == chaseMusic || !playChaseMusic && audioSrc.clip == spyMusic)
+            return;
+
+        //Change song
+        audioSrc.Stop();
+        if (playChaseMusic) audioSrc.clip = chaseMusic;
+        else audioSrc.clip = spyMusic;
+        audioSrc.Play();
     }
 }
