28cd6477198cc49941418df5a6e817cccd3a54c5
Niko-Viitio <dnivi009@edu.xamk.fi>
2021-11-07 11:28:03 +0200

    flashlight and now you can leave museum

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,9 +28,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            EndGame();
-
         if (gameStarted)
         {
             //Timer
